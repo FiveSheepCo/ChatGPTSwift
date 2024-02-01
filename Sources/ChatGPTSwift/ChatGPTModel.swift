70ca60f9793eb750f45cb5624ddfb9f43ed896a6
SplittyDev <splittydev@protonmail.com>
2024-02-01 21:06:45 +0100

    Add new GPT 3.5 Turbo and GPT 4 Turbo models

--- Sources/ChatGPTSwift/ChatGPTModel.swift
+++ Sources/ChatGPTSwift/ChatGPTModel.swift
@@ -12,12 +12,15 @@ public enum ChatGPTModel {
     // GPT-4
     case gpt4_turbo_preview
     case gpt4_turbo_vision_preview
+    case gpt4_turbo_0125_preview
+    case gpt4_turbo_1106_preview
     case gpt4
     case gpt4_32k
     case gpt4_0613
     case gpt4_32k_0613
     
     // GPT-3.5
+    case gpt3_5_turbo_0125
     case gpt3_5_turbo_1106
     case gpt3_5_turbo
     case gpt3_5_turbo_16k
@@ -28,9 +31,13 @@ extension ChatGPTModel {
     var modelName: String {
         switch self {
             case .gpt4_turbo_preview:
-                "gpt-4-1106-preview"
+                "gpt-4-turbo-preview"
             case .gpt4_turbo_vision_preview:
                 "gpt-4-vision-preview"
+            case .gpt4_turbo_0125_preview:
+                "gpt-4-0125-preview"
+            case .gpt4_turbo_1106_preview:
+                "gpt-4-1106-preview"
             case .gpt4:
                 "gpt-4"
             case .gpt4_32k:
@@ -39,6 +46,8 @@ extension ChatGPTModel {
                 "gpt-4-0613"
             case .gpt4_32k_0613:
                 "gpt-4-32k-0613"
+            case .gpt3_5_turbo_0125:
+                "gpt-3.5-turbo-0125"
             case .gpt3_5_turbo_1106:
                 "gpt-3.5-turbo-1106"
             case .gpt3_5_turbo:
@@ -52,13 +61,13 @@ extension ChatGPTModel {
     
     var contextWindow: Int {
         switch self {
-            case .gpt4_turbo_preview, .gpt4_turbo_vision_preview:
+            case .gpt4_turbo_preview, .gpt4_turbo_vision_preview, .gpt4_turbo_0125_preview, .gpt4_turbo_1106_preview:
                 128_000
             case .gpt4, .gpt4_0613:
                 8_192
             case .gpt4_32k, .gpt4_32k_0613:
                 32_768
-            case .gpt3_5_turbo_1106, .gpt3_5_turbo_16k:
+            case .gpt3_5_turbo_1106, .gpt3_5_turbo_0125, .gpt3_5_turbo_16k:
                 16_385
             case .gpt3_5_turbo, .gpt3_5_turbo_instruct:
                 4_096
