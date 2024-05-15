9d6eeb74a39260a1eeeaf6beec34b12f6cd49178
SplittyDev <splittydev@gmail.com>
2024-05-15 14:57:35 +0700

    Add support for GPT-4o and custom models
    Also adds support for `gpt-4-turbo`.

--- Sources/ChatGPTSwift/ChatGPTModel.swift
+++ Sources/ChatGPTSwift/ChatGPTModel.swift
@@ -7,29 +7,52 @@
 
 import Foundation
 
+public struct CustomModel {
+    public let name: String
+    public let contextWindow: Int
+    
+    public init(name: String, contextWindow: Int) {
+        self.name = name
+        self.contextWindow = contextWindow
+    }
+}
+
 public enum ChatGPTModel {
     
-    // GPT-4
+    // GPT-4 Omni
+    case gpt4o
+    
+    // GPT-4 Turbo
+    case gpt4_turbo
     case gpt4_turbo_preview
     case gpt4_turbo_vision_preview
     case gpt4_turbo_0125_preview
     case gpt4_turbo_1106_preview
+    
+    // GPT-4
     case gpt4
     case gpt4_32k
     case gpt4_0613
     case gpt4_32k_0613
     
-    // GPT-3.5
+    // GPT-3.5 Turbo
     case gpt3_5_turbo_0125
     case gpt3_5_turbo_1106
     case gpt3_5_turbo
     case gpt3_5_turbo_16k
     case gpt3_5_turbo_instruct
+    
+    // Custom
+    case custom(CustomModel)
 }
 
 extension ChatGPTModel {
     var modelName: String {
         switch self {
+            case .gpt4o:
+                "gpt-4o"
+            case .gpt4_turbo:
+                "gpt-4-turbo"
             case .gpt4_turbo_preview:
                 "gpt-4-turbo-preview"
             case .gpt4_turbo_vision_preview:
@@ -56,12 +79,14 @@ extension ChatGPTModel {
                 "gpt-3.5-turbo-16k"
             case .gpt3_5_turbo_instruct:
                 "gpt-3.5-turbo-instruct"
+            case .custom(let model):
+                model.name
         }
     }
     
     var contextWindow: Int {
         switch self {
-            case .gpt4_turbo_preview, .gpt4_turbo_vision_preview, .gpt4_turbo_0125_preview, .gpt4_turbo_1106_preview:
+            case .gpt4o, .gpt4_turbo, .gpt4_turbo_preview, .gpt4_turbo_vision_preview, .gpt4_turbo_0125_preview, .gpt4_turbo_1106_preview:
                 128_000
             case .gpt4, .gpt4_0613:
                 8_192
@@ -71,6 +96,8 @@ extension ChatGPTModel {
                 16_385
             case .gpt3_5_turbo, .gpt3_5_turbo_instruct:
                 4_096
+            case .custom(let model):
+                model.contextWindow
         }
     }
 }
